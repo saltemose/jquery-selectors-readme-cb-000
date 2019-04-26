6a378e7ad307f31c9ca6462cb604cfe17b77bf96
Sean Altemose <sean@seanaltemose.com>
2019-04-26 18:04:22 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+    var paragraphs = $('p');
+    return paragraphs
+};
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage
+};
+
+function ninjaBabySelector(){
+  var image = $('#baby-ninja');
+  return image
+};
+
+function divSelector(){
+  var divs = $('div.pics');
+  return divs
+};
+
+function firstListItem(){
+  var item = $('#pic-list li:first');
+  return item
+};
